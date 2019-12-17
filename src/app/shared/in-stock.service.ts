04c7cd8cef03f1201453b076b207afec6e035f1a
Shika <loka.eslam@hotmail.com>
2019-12-17 22:12:28 +0200

    Improving filtering functionality

--- src/app/shared/in-stock.service.ts
+++ src/app/shared/in-stock.service.ts
@@ -52,9 +52,9 @@ export class InStockService {
         );
       }
     }
+    console.log(filtered);
 
-    this.filteredItems = filtered;
-    this.categoryLoader.next(filtered);
+    this.loadCategoryItems(filtered);
       
   }
 

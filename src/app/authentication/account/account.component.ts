a0d21c0461381050eb2c4f5ef5ac379f19623f4e
Shika <loka.eslam@hotmail.com>
2020-02-26 05:03:35 +0200

    Fixed deactivating account comp

--- src/app/authentication/account/account.component.ts
+++ src/app/authentication/account/account.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../../shared/auth.service';
-import { take, concatMap } from "rxjs/operators";
+import { take, elementAt } from "rxjs/operators";
 import { DataService } from "../../shared/data.service";
 import { UserInfo } from '../../shared/userInfo.model';
 import { Subscription, Observable } from 'rxjs';
@@ -18,6 +18,7 @@ export class AccountComponent implements OnInit,OnDestroy, CanComponentDeactivat
   signupForm: FormGroup;
   formChanged: boolean;
   formSub: Subscription;
+  formChanges: Subscription;
 
   constructor(
     private route: ActivatedRoute, 
@@ -26,14 +27,14 @@ export class AccountComponent implements OnInit,OnDestroy, CanComponentDeactivat
     ) {
       this.signupForm = new FormGroup({
         personalForm: new FormGroup({
-          "firstName": new FormControl("Hassan", [this.validateString.bind(this)]),
-          "lastName": new FormControl("Yehia", [this.validateString.bind(this)]),
-          "companyName": new FormControl("Mahgoub"),
-          "gender": new FormControl("male"),
-          "address": new FormControl("Masakn"),
-          "city": new FormControl("Cairo"),
-          "country": new FormControl("Egypt"),
-          "phoneno": new FormControl("201115909716", [Validators.minLength(12), this.numberValidator.bind(this)]),
+          "firstName": new FormControl(null, [this.validateString.bind(this)]),
+          "lastName": new FormControl(null, [this.validateString.bind(this)]),
+          "companyName": new FormControl(null),
+          "gender": new FormControl(null),
+          "address": new FormControl(null),
+          "city": new FormControl(null),
+          "country": new FormControl(null),
+          "phoneno": new FormControl("", [Validators.minLength(12), this.numberValidator.bind(this)]),
         }),
         authForm: new FormGroup({
           "password": new FormControl(null, Validators.minLength(8)),
@@ -46,21 +47,24 @@ export class AccountComponent implements OnInit,OnDestroy, CanComponentDeactivat
     this.editMode = !!this.route.snapshot.fragment;
     if (this.editMode) {
       this.formSub = this.dataService.getUserInfo()
-        .pipe(
-          take(1),
-          concatMap(
-            (userInfo: UserInfo) => {
-              this.populateData(userInfo);
-              this.formChanged = false;
-              return this.signupForm.valueChanges
-            }
-          )
-        )
-        .subscribe(
-          _ => {
-            this.formChanged = true;
-          }
-        )
+                      .pipe(
+                        take(1)
+                      )
+                      .subscribe(
+                        (userInfo: UserInfo) => {
+                          this.populateData(userInfo);
+                          this.formChanged = false;
+                        }
+                      );
+      this.formChanges = this.signupForm.valueChanges
+                          .pipe(
+                            elementAt(3)
+                          )
+                          .subscribe(
+                            _ => {
+                              this.formChanged = true;
+                            }
+                          )
     }
     
   }
@@ -73,7 +77,8 @@ export class AccountComponent implements OnInit,OnDestroy, CanComponentDeactivat
   }
 
   private numberValidator(control: FormControl): {[s: string]: boolean} {
-    if (isNaN(control.value) || !control.value.startsWith("20")) {
+    let inputValue = control.value || " ";
+    if (isNaN(control.value) || !inputValue.startsWith("20")) {
       return { invalid: true }
     }
 
@@ -87,8 +92,6 @@ export class AccountComponent implements OnInit,OnDestroy, CanComponentDeactivat
   }
 
   onSubmit() {
-    this.formChanged = false;
-    this.signupForm.reset();
     if (this.editMode) {
       const newInfo = this.signupForm.get("personalForm").value;
       this.dataService.updateUserInfo(newInfo)
@@ -97,7 +100,7 @@ export class AccountComponent implements OnInit,OnDestroy, CanComponentDeactivat
           _ => {
             console.log(this.signupForm.get("personalForm"));
           }
-        )
+          )
     } else {
       const username = `${this.signupForm.get("personalForm.firstName").value} ${this.signupForm.get("personalForm.lastName").value}`;
       const personalInfo = this.signupForm.get('personalForm').value;
@@ -108,11 +111,15 @@ export class AccountComponent implements OnInit,OnDestroy, CanComponentDeactivat
           res => {
             console.log(res);
           }
-        )
+          )
     }
+
+      this.formChanged = false;
+      this.signupForm.reset();
   }
 
   canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    console.log(this.formChanged);
     return this.formChanged ? confirm("Discard Changes?!") : true;
   }
 
